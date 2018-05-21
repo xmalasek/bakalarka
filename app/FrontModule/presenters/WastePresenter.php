b8d6a5c66014a53cfc91a5d835b5433885fcac03
Adam Malasek <xmalasek@mendelu.cz>
2018-05-21 13:16:58 +0200

    uprava front_end

--- app/FrontModule/presenters/WastePresenter.php
+++ app/FrontModule/presenters/WastePresenter.php
@@ -7,16 +7,65 @@ use Nette;
 
 class WastePresenter extends Nette\Application\UI\Presenter
 {
-    public function __construct()
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
     {
         parent::__construct();
+        $this->database = $database;
+
     }
 
-    /**
-     *
-     */
     public function renderDefault()
     {
+        $this->template->waste = $this->database->table('waste');
+    }
+
+    public function renderShow()
+    {
+        $this->template->waste = $this->database->table('waste');
+
+
+    }
+
+    public function renderFault()
+    {
+
+
+    }
+
+
+    public function actionFault($id){
+
+        $this['insertFaultForm']->setDefaults([
+            'id_device' => $id
+        ]);
+
+    }
+
+    protected function createComponentInsertFaultForm(){
+
+        $form = (new InsertFaultFormFactory()) -> create();
+        $form->onSuccess[] = [$this, 'insertWasteFaultSucceeded'];
+        return $form;
+
+    }
+
+    public function insertWasteFaultSucceeded($form, $values){
+
+        $data=
+            ['description' => $values->description ,
+                'datum' => $values->datum];
+
+        $error_id = $this->database->table('error')->insert($data)->id_error;
+
+        $this->database->table('waste')
+            ->where('id_waste', $values->id_device)
+            ->update([
+                'error_id' => $error_id
+            ]);
+
+        $this->redirect('Waste:default');
 
     }
 }
